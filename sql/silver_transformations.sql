49f3ef700698b4f42cc85c4c8bf5c47033ee8ee4
JMK-104 <JMK-104>
2025-10-10 15:34:12 +0000

    update silver_transformations.sql

--- sql/silver_transformations.sql
+++ sql/silver_transformations.sql
@@ -9,32 +9,33 @@ Silver Transformations:
     - Change date_posted column from TIMESTAMP to DATE datatype
 */
 
-SELECT 
-  TRIM(id) AS id,
-  TRIM(role) AS role,
-  TRIM(company_name) AS company_name,
-  CASE
-    WHEN company_num_employees IS NULL THEN 'N/A'
-    ELSE TRIM(company_num_employees)
-  END AS company_num_employees,
-  CASE
-    WHEN employment_type IS NULL THEN 'N/A'
-    ELSE TRIM(employment_type)
-  END AS employment_type,
-  CASE
-    WHEN location IS NULL THEN 'N/A'
-    ELSE TRIM(location)
-  END AS location,
-  TRIM(remote) AS remote,
-  CASE
-    WHEN logo IS NULL THEN 'N/A'
-    ELSE TRIM(logo)
-  END AS logo,
-  TRIM(url) AS url,
-  CASE
-    WHEN text IS NULL THEN 'N/A'
-    ELSE TRIM(text)
-  END AS text,
-  CAST(date_posted AS DATE) AS date_posted
-FROM bronze.jobs_table
+CREATE OR REPLACE TABLE silver.jobs_table AS
+  SELECT 
+    TRIM(id) AS id,
+    TRIM(role) AS role,
+    TRIM(company_name) AS company_name,
+    CASE
+      WHEN company_num_employees IS NULL THEN 'N/A'
+      ELSE TRIM(company_num_employees)
+    END AS company_num_employees,
+    CASE
+      WHEN employment_type IS NULL THEN 'N/A'
+      ELSE TRIM(employment_type)
+    END AS employment_type,
+    CASE
+      WHEN location IS NULL THEN 'N/A'
+      ELSE TRIM(location)
+    END AS location,
+    TRIM(remote) AS remote,
+    CASE
+      WHEN logo IS NULL THEN 'N/A'
+      ELSE TRIM(logo)
+    END AS logo,
+    TRIM(url) AS url,
+    CASE
+      WHEN text IS NULL THEN 'N/A'
+      ELSE TRIM(text)
+    END AS text,
+    CAST(date_posted AS DATE) AS date_posted
+  FROM bronze.jobs_table
 ;
