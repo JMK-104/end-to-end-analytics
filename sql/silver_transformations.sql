6ec54f7501d2402b386e0c89f7ae3de9e6b415cf
JMK-104 <JMK-104>
2025-10-10 15:22:24 +0000

    update silver_transformaitons.sql

--- sql/silver_transformations.sql
+++ sql/silver_transformations.sql
@@ -4,23 +4,37 @@ Silver Transformations:
   and prepare it for later transformations in the gold layer
   
   Transformations Performed:
-    - Remove NULL values from employment_type column
-    - Remove NULL values from location column 
+    - Remove NULL values
+    - Remove White Spaces
     - Change date_posted column from TIMESTAMP to DATE datatype
 */
+
 SELECT 
-  id,
-  role,
-  company_name,
+  TRIM(id) AS id,
+  TRIM(role) AS role,
+  TRIM(company_name) AS company_name,
+  CASE
+    WHEN company_num_employees IS NULL THEN 'N/A'
+    ELSE TRIM(company_num_employees)
+  END AS company_num_employees,
   CASE
     WHEN employment_type IS NULL THEN 'N/A'
-    ELSE employment_type
+    ELSE TRIM(employment_type)
   END AS employment_type,
   CASE
     WHEN location IS NULL THEN 'N/A'
-    ELSE location
+    ELSE TRIM(location)
   END AS location,
-  remote,
+  TRIM(remote) AS remote,
+  CASE
+    WHEN logo IS NULL THEN 'N/A'
+    ELSE TRIM(logo)
+  END AS logo,
+  TRIM(url) AS url,
+  CASE
+    WHEN text IS NULL THEN 'N/A'
+    ELSE TRIM(text)
+  END AS text,
   CAST(date_posted AS DATE) AS date_posted
 FROM bronze.jobs_table
 ;
